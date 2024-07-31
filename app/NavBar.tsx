22e92c861204e5df69dcba99d14de90b23774aef
efexng <efeapoki2020@gmail.com>
2024-07-31 14:56:33 +0100

    building an api

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
+import classnames from 'classnames'
 
 const NavBar = () => {
+   const currentPath = usePathname();
 
     const links = [
-        {name: 'Dashboard', href: '/'},
-        {name: 'Issues', href: '/issues'},
+        {label: 'Dashboard', href: '/'},
+        {label: 'Issues', href: '/issues'},
         // Add more links here...
   
     ]
@@ -14,12 +19,15 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><AiFillBug /></Link>
         <ul className='flex space-x-6'>
-            {links.map((link, index) => (
-                <li key={index}>
-                    <Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.name}</Link>
-                </li>
-            ))}
-            <li></li>
+          {links.map(link =>
+          <Link
+            key={link.href}
+            className={classnames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+            })}
+            href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
