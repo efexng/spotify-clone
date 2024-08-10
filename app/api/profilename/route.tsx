cc260c183c17e0b9c4d2ffd5f9d605caa9f12e64
efexng <efeapoki2020@gmail.com>
2024-08-10 03:07:03 +0100

    editprofile

--- app/api/profilename/route.tsx
+++ app/api/profilename/route.tsx
@@ -3,20 +3,25 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
 
 export async function GET(request: NextRequest) {
-  // Here you would normally get the user ID from the session or JWT
-  const userId = 1; // Replace with dynamic user ID retrieval based on authentication
+  const userId = 1; 
 
   try {
     const user = await prisma.user.findUnique({
       where: { id: userId },
-      select: { profileName: true },
+      select: { 
+        profileName: true,
+        emailOrPhone: true, // Include emailOrPhone in the selection
+      },
     });
 
     if (!user) {
       return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
 
-    return NextResponse.json({ profileName: user.profileName }, { status: 200 });
+    return NextResponse.json({ 
+      profileName: user.profileName, 
+      emailOrPhone: user.emailOrPhone, // Return emailOrPhone in the response
+    }, { status: 200 });
   } catch (error) {
     console.error('Error fetching user profile:', error);
     return NextResponse.json({ error: 'An error occurred while fetching the user profile' }, { status: 500 });
